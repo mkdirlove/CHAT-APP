4ef7e393914c5afd9a92994e938b59ac875e30b0
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-04-11 17:19:16 -0500

    Se mejoro codigo

--- v2/chat_server.py
+++ v2/chat_server.py
@@ -1,4 +1,7 @@
-import sys, time, socket, threading                                         
+#!/usr/bin/python3
+
+from os import system
+import sys, time, socket, threading, platform                                         
 
 class bcolors:
     HEADER = '\033[95m'
@@ -23,6 +26,7 @@ banner = """
 
 [+] Welcome to our simple chatroom [+]
 """
+
 host = '127.0.0.1'                                                      
 port = 12345                                                     
 
@@ -32,6 +36,14 @@ server.listen()
 
 clients = []
 nicknames = []
+
+
+def clear_pan():
+    if platform.system() == "Linux":
+        system("clear")
+    else:
+        exit(1)
+
 
 def banner_slowprint(s):
 	for c in s + '\n':
@@ -59,27 +71,32 @@ def handle(client):
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
-            broadcast('{} left in the chatroom!\n'.format(nickname).encode('ascii'))
+            broadcast("{} left in the chatroom!\n".format(nickname).encode('ascii'))
             nicknames.remove(nickname)
             break
 
 def receive():                                                          
     while True:
-        client, address = server.accept()
-        slowprint(bcolors.GREEN+"[CONNECTED] Connected with {}".format(str(address)))       
-        client.send('NICKNAME'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
-        slowprint("[CLIENT] {}".format(nickname))
-        broadcast("[CLIENT] {} joined in the chatroom!".format(nickname).encode('ascii'))
-        #client.send('[!] Connected to the server...\n'.encode('ascii'))
-        #client.send('\n[+] You can now start chatting...'.encode('ascii'))
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
+        try:
+            client, address = server.accept()
+            slowprint(f"{bcolors.GREEN}[CONNECTED] Connected with {str(address)}")       
+            client.send('NICKNAME'.encode('ascii'))
+            nickname = client.recv(1024).decode('ascii')
+            nicknames.append(nickname)
+            clients.append(client)
+            broadcast("[CLIENT] {} joined in the chatroom!".format(nickname).encode('ascii'))
+            #client.send('[!] Connected to the server...\n'.encode('ascii'))
+            #client.send('\n[+] You can now start chatting...'.encode('ascii'))
+            thread = threading.Thread(target=handle, args=(client,))
+            thread.start()
+        except KeyboardInterrupt:
+            slowprint(f"{bcolors.FAIL}[ERROR] error he was forced{bcolors.ENDC}")
+            exit(1)
 
-banner_slowprint(bcolors.HEADER+banner+bcolors.ENDC)
-slowprint(bcolors.WARNING+"[STARTING] Server is starting..."+bcolors.ENDC)
-slowprint(bcolors.GREEN+"[STARTED] Server is up and running..."+bcolors.ENDC)
-slowprint(bcolors.BLUE+"[WAITING] Waiting for clients to connect..."+bcolors.ENDC)
-receive()
+if __name__ == '__main__':
+    clear_pan()
+    banner_slowprint(bcolors.HEADER+banner+bcolors.ENDC)
+    slowprint(f"{bcolors.WARNING}[STARTING] Server is starting...{bcolors.ENDC}")
+    slowprint(f"{bcolors.GREEN}[STARTED] Server is up and running...{bcolors.ENDC}")
+    slowprint(f"{bcolors.BLUE}[WAITING] Waiting for clients to connect...{bcolors.ENDC}")
+    receive()
